18390522240a4351219d6ab3b64dd6497c2c350f
Kathleen_M <kathleen.malialin@gmail.com>
2019-12-28 13:37:24 -0300

    trsfr-site

---
+++ script.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+  alert("Bienvenue ! Pour votre confort visuel, vous pouvez activer le mode nuit en cliquant sur la pastille en bas à droite de votre écran. Enjoy ! :)");
+})
